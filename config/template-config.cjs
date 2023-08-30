221bbc7cc654d308af3dfb55e5313abffbac96ac
Clement-HUANG <102946149+Clement-HUANG@users.noreply.github.com>
2023-08-30 16:53:02 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -2,37 +2,42 @@
 const TEMPLATE_CONFIG = [
   {
     id: '0001',
-    title: '亲爱的, 早上好',
+    title: '早上好鸭我的憨',
     desc: `
       **{{date.DATA}}**
       
-      下个休息日：{{holidaytts.DATA}}
-      
+            
       ---
       
       城市：{{city.DATA}}
       
-      天气：{{weather.DATA}}
+      ☁️ 天气：{{weather.DATA}}
       
-      气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
+      🌡️ 气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
       
       风向: {{wind_direction.DATA}}
       
       风级: {{wind_scale.DATA}}
       
-      {{comprehensive_horoscope.DATA}}
       
       ---
       
-      今天是我们相识的第{{love_day.DATA}}天
+      🗓️今天是我们相识的第{{love_day.DATA}}天
       
-      {{birthday_message.DATA}}
+      💗 {{birthday_message.DATA}}
       
       ---
       
-      {{moment_copyrighting.DATA}}
+      💌{{earthy_love_words.DATA}} 
       
-      {{poetry_title.DATA}} {{poetry_content.DATA}}
+      💌{{love_horoscope.DATA}} 
+      
+      💌{{healthy_horoscope.DATA}} 
+      
+      💌{{note_en.DATA}} 
+
+      
+   
     `
   },
   {
